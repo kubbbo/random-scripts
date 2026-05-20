23c07eb7b9eb90701a655a5ebc1586ba592c6202
kubbbo <luptacik.jakub@gmail.com>
2026-05-20 11:05:19 +0200

    Generalize template

--- scripts/TEMPLATE.sh
+++ scripts/TEMPLATE.sh
@@ -6,16 +6,16 @@ function RENAME()
 
 	while getopts "h" ARG; do
 		case "$ARG" in
-			h) echo -en "Usage:\ndcdate [-h] dd mm yy hh:mm\n"
+			h) echo -en "Usage:\n [-h] \n"
 			   echo "    -h - show help"
-			   return 1;;
+			   return 0;;
 			*) echo "Invalid option, use with -h.">&2 && return 1;;
 		esac
 	done
 
 	shift $((OPTIND - 1))
 
-	if [ $# -ne 4 ]
+	if [ $# -ne _ ]
 	then echo -en "Incorrect number of parameters, $# of 4.\nUse with -h for help.\n">&2 && return 1
 	fi
 	
